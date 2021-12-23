7c2e2c903ababfa72097377d1a0ed46cac3f9c32
darh-jorge <jorge@foxlabs.io>
2021-12-23 09:50:36 -0500

    feat: adding new contact page empty

--- lib/todo_test_web/controllers/page_controller.ex
+++ lib/todo_test_web/controllers/page_controller.ex
@@ -4,4 +4,9 @@ defmodule TodoTestWeb.PageController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
+
+  # declarar funcion
+  def contact(conn, _params) do
+    render(conn, "contact.html")
+  end
 end
